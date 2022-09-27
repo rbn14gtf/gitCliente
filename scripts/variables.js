e3dabf7eeb2c69762bc74cdb23035aea3ac6636c
rbn14_gtf <3a.ruben.garciarivera@gmail.com>
2022-09-27 09:44:08 +0200

    fin clase 27/09

--- scripts/variables.js
+++
@@ -1,9 +0,0 @@
-let altura=180;
-{
-    let peso=80;
-    let altura=200;
-    console.log(altura);//200
-    console.log(peso);//80
-}
-console.log(altura);//180
-console.log(peso);//variable no declarada
