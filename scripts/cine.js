63d1c715854bf2780f54f4b5de702fffc606a8e5
rbn14_gtf <3a.ruben.garciarivera@gmail.com>
2022-09-29 09:01:17 +0200

    cine

--- scripts/cine.js
+++ scripts/cine.js
@@ -1,6 +1,9 @@
-function Persona(name,age){
+function Persona(name,age,peliculaElegida,precioEntrada,regalosRecibidos){
     this.name=name;
     this.age=age;
+    this.peliculaElegida=peliculaElegida;
+    this.precioEntrada=precioEntrada;
+    this.regalosRecibidos=regalosRecibidos;
 }
 
 function esMayorEdad(edad){
