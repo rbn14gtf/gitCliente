5d88d73ebadad72854024499ff20876ede5b4615
rbn14_gtf <3a.ruben.garciarivera@gmail.com>
2022-10-04 08:28:12 +0200

    cine DEF

--- scripts/cine.js
+++ scripts/cine.js
@@ -14,3 +14,19 @@ function esMayorEdad(edad){
     }
 
 }
+function getDescuento(edad){
+    if (edad<=17){
+        return 0.5;
+    }else if((edad>=18)&&(edad<=25)){
+        return 1;
+    }else if((edad>=26)&&(edad<=35)){
+        return 0.9;
+    }
+    else if(edad>=36){
+        return 0.8;
+    }
+    else{
+        return 1;
+    }
+
+}
