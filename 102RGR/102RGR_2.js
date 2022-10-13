70eaca07f7c4ea8083a2e3d9b9afe3ac7da830a7
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-10-13 09:39:54 +0200

    102RGR_2

---
+++ 102RGR/102RGR_2.js
@@ -0,0 +1,8 @@
+let contactos=[{nombre:"Maxwell Wright",telefono:"(0191) 7196495",correo:"Curabitur.egestas.nunc@nonummyac.co.uk"},{nombre:"Raja Villareal",telefono:"0866 398 2895",correo:"posuere.vulputate@sed.com"},{nombre:"Helen Richards",telefono:"0800 1111",correo:"libero@convallis.edu"},{nombre:"Maisie Haley",telefono:"0913 531 3030",correo:"risus.Quisque@urna.ca"}];
+
+contactos.push({nombre:"Maisie Haley",telefono:"0913 531 3030",correo:"risus.Quisque@urna.ca"});
+
+console.log(contactos[0]);
+console.log(contactos[contactos.length-1]);
+alert(contactos[0].nombre+" / "+contactos[0].telefono+" / "+contactos[0].correo);   
+alert(contactos[contactos.length-1].nombre+" / "+contactos[contactos.length-1].telefono+" / "+contactos[contactos.length-1].correo);   
