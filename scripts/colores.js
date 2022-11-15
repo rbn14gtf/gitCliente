1ae9e0b8c8bbd8d95b230272f8269e5c3ba4f260
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-11-15 08:38:04 +0100

    colores

--- scripts/colores.js
+++ scripts/colores.js
@@ -3,25 +3,18 @@ function rojo(){
     document.getElementById("redButton").style.backgroundColor = "red";
     console.log("Color de fondo cambiado a rojo")
     document.getElementById("color").innerHTML="Rojo"
-
-
 }
 function amarillo(){
     document.body.style.backgroundColor = "yellow";
     document.getElementById("yellowButton").style.backgroundColor = "yellow";
     console.log("Color de fondo cambiado a amarillo")
     document.getElementById("color").innerHTML="Amarillo"
-
-
-    
 }
 function verde(){
     document.body.style.backgroundColor = "green";
     document.getElementById("greenButton").style.backgroundColor = "green";
     console.log("Color de fondo cambiado a verde")
     document.getElementById("color").innerHTML="Verde"
-
-
 }
 function limpiar(){
     location.reload();
