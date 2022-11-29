dd69254c06b2cf1f16a243f5322fdee2c1269a64
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-11-29 08:51:21 +0100

    ejercicioDOM

--- buscaminasRGR/buscaminas.js
+++ buscaminasRGR/buscaminas.js
@@ -1,14 +1,14 @@
 
-function generar(){
+function generarTablero(){
     let numFilas=10;
     let numBombas= document.getElementById("numBombas").value;
     document.open()
     document.write("<div class='tablero'><table><tbody>")
     let cont=0;
     for(let i=0;i<numFilas;i++){
-        document.write("<tr>")
+        document.write("<tr class='td' style='border: 1px solid black width: 1.5em height: 1.5em;'>")
         for(let j=0;j<numFilas;j++){
-            document.write("<td id="+cont+">["+cont+"]</td>")
+            document.write("<td class='td' style='border: 1px solid black; width: 1.5em; height: 1.5em;' id="+cont+"></td>")
             cont++;
         }
         document.write("</tr>")
