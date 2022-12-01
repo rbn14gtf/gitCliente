69f6a19e0ac44724f89d768483ce9d572443af34
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-12-01 10:33:15 +0100

    buscaminasRGR

--- buscaminasRGR/buscaminas.js
+++ buscaminasRGR/buscaminas.js
@@ -2,18 +2,18 @@ const numFilas=10;
 function tdClick(id){ 
     document.getElementById(id).setAttribute("class","destapado")
 };  
-function generarBombas(){ 
+function generarMinas(){ 
     //let numBombas=document.getElementById("numBombas").value
     for(let i=0;i<100;i+=10){
         posBomba=i+Math.round(Math.random() * 10);
-        document.getElementById(posBomba).setAttribute("class","bomba")
+        document.getElementById(posBomba).setAttribute("class","mina")
         console.log(posBomba)
     }
 
     hideInput()
 }
 function hideInput(){
-    var x = document.getElementById("inputBombas");
+    var x = document.getElementById("inputMinas");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
