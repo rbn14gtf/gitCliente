84f30ca533ba03449518db546bbdc034614ffb48
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-12-13 08:20:06 +0100

    Ejercicios Extra

--- Ejercicios Extra/Mouse Position/mousePosition.js
+++ Ejercicios Extra/Mouse Position/mousePosition.js
@@ -1,6 +1,6 @@
 function getMouseCordenadas(e) {
-	var x = e.clientX;
-	var y = e.clientY;
-	document.getElementById("idInfoX").innerHTML = e.clientX;
-	document.getElementById("idInfoY").innerHTML = e.clientY;
+    var x = e.clientX;
+    var y = e.clientY;
+    document.getElementById("idInfoX").innerHTML = e.clientX;
+    document.getElementById("idInfoY").innerHTML = e.clientY;
 }
