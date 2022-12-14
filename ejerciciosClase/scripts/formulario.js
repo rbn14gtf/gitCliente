36bfc70935c49dead3ed51a9a30b872093b4557f
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-12-14 09:26:18 +0100

    formulario

--- ejerciciosClase/scripts/formulario.js
+++ ejerciciosClase/scripts/formulario.js
@@ -15,5 +15,43 @@ function iniciar(){
 }
 function validar(){
     console.log("hola")
+    if(isValidName()&&isValidPhone()&&isMayorChecked()&&isSexoSelected){
+        return true
+    }else{
+        return false
+    }
 
+}
+
+function isValidName(){
+    if(document.getElementById("nombre").value==""){
+        return false
+    }else{
+        return true
+    }
+}
+function isValidPhone(){
+    if(document.getElementById("telefono").value==""){
+        return false
+    }else{
+        return true
+    }
+}
+function isMayorChecked(){
+    if(document.getElementById("mayor").checked){
+        return true
+    }else{
+        return false
+    }
+}
+function isSexoSelected(){
+    var radios = document.getElementsByName("sexo");
+
+    for (var i = 0, len = radios.length; i < len; i++) {
+        if (radios[i].checked) {
+            return true;
+        }
+    }
+
+    return false;
 }
