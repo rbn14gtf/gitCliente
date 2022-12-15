b3519aead879c68f344f945ce8f2da9ef85db929
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-12-15 17:02:53 +0100

    buscaminasRGR

--- buscaminasRGR/jjgl/buscaminasJJGL.js
+++ buscaminasRGR/jjgl/buscaminasJJGL.js
@@ -3,6 +3,7 @@ let arrayMinas=[]
 function inicio(){
     mostrarTablero()
     generarMinas()
+    // soloMinasMenosUna()
 }
 function mostrarTablero(){
     let tabla=document.createElement("table")
@@ -20,11 +21,22 @@ function crearCasillero(fila,i,j){
     let casilla=document.createElement("td")
     casilla.setAttribute("id",10*i+j)
     casilla.setAttribute("onclick","tdClick(id)")
-    // casilla.innerHTML=10*i+j+1
-    casilla.style.backgroundColor="grey"
+    // casilla.innerHTML=10*i+j
+    // casilla.style.backgroundColor="grey"
     fila.appendChild(casilla)
 }
+function soloMinasMenosUna(){
+    for(let i=0;i<100;i++){
+        if(document.getElementById(i).getAttribute("class")=="minaOculta"){
 
+        }else{
+            document.getElementById(i).setAttribute("class","destapado")
+            // document.getElementById(i).innerHTML=i
+
+        }
+    }
+    document.getElementById(99).removeAttribute("class")
+}
 function generarMinas(){ 
     arrayMinas=[]
     for(let i=0;i<100;i+=10){
@@ -67,6 +79,7 @@ function tdClick(id){
         else{
             document.getElementById(id).setAttribute("class","destapado")
             console.log(getCasillasLimpiasPorDesmarcar())
+            cleanCasilla(Number(id))
             if(getCasillasLimpiasPorDesmarcar()==0){
                 finDelJuego()
             }
@@ -74,6 +87,50 @@ function tdClick(id){
     }
 };  
 
+function cleanCasilla(id){
+    let numMinasCasilla=0
+    if(id-1>=0){
+        if(document.getElementById(id-1).getAttribute("class")=="minaOculta"){//izquierda
+            numMinasCasilla++
+        }
+    }
+    if(id+1<100){
+        if(document.getElementById(id+1).getAttribute("class")=="minaOculta"){//derecha
+            numMinasCasilla++
+        }
+    }
+    if(id+10<100){//abajo
+        if(document.getElementById(id+10).getAttribute("class")=="minaOculta"){
+            numMinasCasilla++
+        }
+    }
+    if(id-10>=0){//arriba
+        if(document.getElementById(id-10).getAttribute("class")=="minaOculta"){
+            numMinasCasilla++
+        }
+    }
+    if(id+9<100){//abajo izquierda
+        if(document.getElementById(id+9).getAttribute("class")=="minaOculta"){
+            numMinasCasilla++
+        }
+    }
+    if(id+11<100){//abajo derecha
+        if(document.getElementById(id+11).getAttribute("class")=="minaOculta"){
+            numMinasCasilla++
+        }
+    }
+    if(id-9>=0){//arriba derecha
+        if(document.getElementById(id-9).getAttribute("class")=="minaOculta"){
+            numMinasCasilla++
+        }
+    }
+    if(id-11>=0){//arriba izquierda
+        if(document.getElementById(id-11).getAttribute("class")=="minaOculta"){
+            numMinasCasilla++
+        }
+    }
+    document.getElementById(id).innerHTML=numMinasCasilla
+}
 function finDelJuego(){
     destaparTodas()
     for(let i=0;i<arrayMinas.length;i++){
