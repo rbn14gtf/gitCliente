76e2393ea688fbc68ea23ea33d05a13153135a87
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-12-20 09:21:30 +0100

    ej1

---
+++ ejerciciosClase/Ejercicios Formularios/ej1.js
@@ -0,0 +1,9 @@
+window.onload=inicio
+
+function inicio(){
+    document.getElementById("google").addEventListener("click",redirigir)
+}
+function redirigir(){
+    window.location.href = "https://google.com";
+}
+
