274d34f862efea21e86876c1bffc60feef637abe
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-12-20 09:23:57 +0100

    ej1

--- ejerciciosClase/Ejercicios Formularios/ej1.js
+++ ejerciciosClase/Ejercicios Formularios/ej1.js
@@ -2,8 +2,41 @@ window.onload=inicio
 
 function inicio(){
     document.getElementById("google").addEventListener("click",redirigir)
+    document.getElementById("enviar").addEventListener("click",validar)
 }
 function redirigir(){
     window.location.href = "https://google.com";
 }
+function validar(e){
+    if((isValidName())&&(isValidPhone())&&(isValidDate())&&(isMayorChecked())&&(isSexoSelected()&&(window.confirm("Deseas enviar el formulario?")))){
+        alert("Validado")
+        e.preventDefault();
 
+        return true
+    }else{
+        alert("No Validado")
+        //evita que se produzca la redireccion
+        e.preventDefault();
+        return false
+    }
+}
+function isValidName(){
+    let nombre=document.getElementById("nombre")
+    if(!nombre.checkValidity()){
+        error(nombre)
+        return false
+    }else{
+        limpiarError(nombre)
+        return true
+    }
+}
+
+
+
+function error(elemento){
+    elemento.className="error"
+    elemento.focus()
+}
+function limpiarError(elemento){
+    elemento.className=""
+}
