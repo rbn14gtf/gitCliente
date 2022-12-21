46343a0b54d5b872770000e1732b8704989443ee
rbn14gtf <3a.ruben.garciarivera@gmail.com>
2022-12-21 08:41:17 +0100

    jjgl2

--- ejerciciosClase/scripts/jjgl2.js
+++ ejerciciosClase/scripts/jjgl2.js
@@ -4,47 +4,50 @@ function iniciar() {
     document.getElementById("enviar").addEventListener('click', validar, false);
 }
 
-function validaNombre() {
-    var elemento = document.getElementById("nombre");
-    if (!elemento.checkValidity()) {
-            return false;
-    }
-    return true;
-}
-
-function validaEdad() {
-    var elemento = document.getElementById("edad");
-    if (!elemento.checkValidity()) {
-       
-        return false;
-    }
-    return true;
-}
-
-function validaTelefono() {
-    var elemento = document.getElementById("telefono");
-    if (!elemento.checkValidity()) {
-      
-        return false;
-    }
-    return true;
-}
-
 function validar(e) {
     borrarError();
     if (validaNombre() && validaEdad() && validaTelefono() && confirm("Pulsa aceptar si deseas enviar el formulario")) {
         return true
     } else {
+        error(elemento)
         e.preventDefault();
         return false;
     }
-}
+}//validar
+function validaNombre() {
+    var elemento = document.getElementById("nombre");
+    if (!elemento.checkValidity()) {
+        error(elemento)
+        return false;
+    }
+    return true;
+}//validaNombre
+
+function validaEdad() {
+    var elemento = document.getElementById("edad");
+    if (!elemento.checkValidity()) {
+        error(elemento)
+
+        return false;
+    }
+    return true;
+}//validaEdad
+
+function validaTelefono() {
+    var elemento = document.getElementById("telefono");
+    if (!elemento.checkValidity()) {
+        error(elemento)
+        return false;
+    }
+    return true;
+}//validaTelefono
+
 
 function error(elemento) {
     document.getElementById("mensajeError").innerHTML = elemento.validationMessage;
     elemento.className = "error";
     elemento.focus();
-}
+}//error
 
 
 
@@ -53,4 +56,4 @@ function borrarError() {
     for (var i = 0; i < formulario.elements.length; i++) {
         formulario.elements[i].className = "";
     }
-}
+}//borrarError
